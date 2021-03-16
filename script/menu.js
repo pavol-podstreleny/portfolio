e02cdf6156fadf6eacb54a5e25a70f10b63a9c25
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-03-16 21:21:28 +0800

    Created slidable menu for mobile devices

--- script/menu.js
+++ script/menu.js
@@ -1,12 +1,14 @@
 const menuButton = document.querySelector("#menu-btn")
-console.log(menuButton)
+const mainMenu = document.querySelector("#main-menu")
 let isMenuOpen = false
 
 menuButton.addEventListener("click",() => {
     if(isMenuOpen){
         menuButton.classList.remove("open")
+        mainMenu.classList.remove("open")
     }else{
         menuButton.classList.add("open")
+        mainMenu.classList.add("open")
     }
     isMenuOpen = !isMenuOpen
 })
