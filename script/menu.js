6edf8041b2e246abdadaa5c3e6e4ee8d2c6c79a1
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-03-16 23:34:28 +0800

    Added basic structure and style for .is() and .work() sections

--- script/menu.js
+++ script/menu.js
@@ -1,14 +1,17 @@
 const menuButton = document.querySelector("#menu-btn")
 const mainMenu = document.querySelector("#main-menu")
+const body = document.querySelector("body")
 let isMenuOpen = false
 
 menuButton.addEventListener("click",() => {
     if(isMenuOpen){
         menuButton.classList.remove("open")
         mainMenu.classList.remove("open")
+        body.classList.remove("open")
     }else{
         menuButton.classList.add("open")
         mainMenu.classList.add("open")
+        body.classList.add("open")
     }
     isMenuOpen = !isMenuOpen
 })
